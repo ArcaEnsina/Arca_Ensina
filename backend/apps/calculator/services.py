9ef52b75a7cf2eeb7b50d9a0eeb46c2c470a9576
LarissaGiovanna <lgiova544@gmail.com>
2026-05-08 21:31:02 -0300

    feat: other calculator functions - função de converter para dosagens por dia - converter prescrição para frequencia

--- backend/apps/calculator/services.py
+++ backend/apps/calculator/services.py
@@ -7,7 +7,7 @@ from django.apps import AppConfig
 3. calculo da suspensão (quanto que a criança deve tomar em ml, baseado na concentração do medicamento)
 """
 
-#1- calcular dose total em miligramas
+#1.1- calcular dose total em miligramas
 def calculate_dosage_mg(prescription, weight, height=None):
     if height:
         """
@@ -19,7 +19,7 @@ def calculate_dosage_mg(prescription, weight, height=None):
         if prescription <= 0 or height <= 0 or weight <= 0:
             raise ValueError("Prescrição, altura e peso devem ser maiores que zero.")
         scm = sqrt((height * weight) / 3600)
-        return prescription * round(scm, 2)
+        return round(prescription * scm, 2)
     else:
         """
         ex: prescrição: 10mg/kg; peso: 15kg
@@ -29,7 +29,30 @@ def calculate_dosage_mg(prescription, weight, height=None):
         if prescription <= 0 or weight <= 0:
             raise ValueError("Prescrição e peso devem ser maiores que zero.")
             # Dosagem em mg = (Dose por mg/kg) x (Peso do paciente kg)
-        return prescription * weight
+        return round(prescription * weight, 2)
+
+
+#1.2- calcular dose por dia
+def prescription_to_frequency(prescription_time):
+    """
+    ex: prescrição: 10mg/kg/dia a cada 6h
+    -> 24h / 6h = 4 vezes por dia
+    """
+    if prescription_time <= 0:
+        raise ValueError("Tempo da prescrição deve ser maior que zero.")
+    frequency_per_day = 24 / prescription_time
+    if type(frequency_per_day) != int:
+        frequency_per_day = round(frequency_per_day) #arredonda o numero para intero mais próximo, ex: 4.5 -> 5
+    return frequency_per_day
+
+def calculate_dosage_per_dose(total_dosage_mg, frequency_per_day):
+    """
+    ex: dose total: 250mg; frequência: 4 vezes por dia
+    -> 250mg / 4 = 62.5mg por dose
+    """
+    if total_dosage_mg <= 0 or frequency_per_day <= 0:
+        raise ValueError("Dosagem e frequência por dia devem ser maiores que zero.")
+    return round(total_dosage_mg / frequency_per_day, 2)
 
 #2- validação da dose calculada
 
