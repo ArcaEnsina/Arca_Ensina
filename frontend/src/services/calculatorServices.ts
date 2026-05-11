addf748166d46b5c37ee999ead76fa7fff9ae892
LarissaGiovanna <lgiova544@gmail.com>
2026-05-11 19:29:03 -0300

    feat(frontend): getMedicationsById function frontend

--- frontend/src/services/calculatorServices.ts
+++ frontend/src/services/calculatorServices.ts
@@ -1,11 +1,16 @@
 import type { Medication, CalculatorFormData, CalculationResult } from "../types/calculator"
 import api from "../services/api"
 
-export { getMedications, calculate };
+export { getMedications, getMedicationById, calculate };
 
 //buscar medicações via api
 function getMedications(): Promise<Medication[]> {
     return api.get("/api/medications").then((response) => response.data);
+}
+
+//buscar id medicações:
+function getMedicationById(id: number): Promise<Medication> {
+    return api.get(`/api/medications/${id}`).then((response) => response.data);
 }
 
 //calcular dosagem via api
