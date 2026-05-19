406cc6b2849061bd83304caf2a3e2ce700b2759e
LarissaGiovanna <lgiova544@gmail.com>
2026-05-18 23:12:48 -0300

    style: calculator banner with medication badges (not finished)

--- frontend/src/components/Calculator/CalculatorBanner.tsx
+++ frontend/src/components/Calculator/CalculatorBanner.tsx
@@ -2,12 +2,15 @@ import MedicationBadge from "../Medications/MedicationBadge";
 
 function CalculatorBanner() {
     return (
-        <div>
-            <section>
+        <div className="flex flex-col gap-3 items-center bg-gray-300 px-6 py-8 rounded-lg h-full justify-center">
+            <section className="flex">
                 <MedicationBadge name="pill" />
                 <MedicationBadge name="tablets" />
                 <MedicationBadge name="pills-bottle" />
                 <MedicationBadge name="syringe" />
+            </section>
+            <section>
+                <p className="bg-blue-500 text-white p-2 rounded-full">Calculadora de Medicamentos</p>
             </section>
         </div>
     );
