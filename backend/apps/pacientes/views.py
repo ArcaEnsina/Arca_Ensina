2a56bcf58288dbb8cd305dc881a7cb1aaa496651
Larissa Giovanna <lgsc@cesar.school>
2026-06-11 17:02:11 +0000

    lint: rufff check

--- backend/apps/pacientes/views.py
+++ backend/apps/pacientes/views.py
@@ -1,8 +1,8 @@
 from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import action
 
 from apps.protocols.models import ProtocolExecution
 from apps.protocols.serializers import ProtocolSuggestionSerializer
